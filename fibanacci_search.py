ba3a81f3cce190b384d168d74fde40e101eb1d66
Malika Axmatova <malikaaxmatova06@gmail.com>
2024-11-18 19:15:50 +0500

    add fibannacci_search.py

--- fibanacci_search.py
+++ fibanacci_search.py
@@ -7,3 +7,20 @@ def fibonacci_search(arr,n,target):
         f2=f1
         f1=f
         f=f2+f1
+    while f>1:
+        i=min(offset+f2,n-1)
+        if arr[i]<target:
+            f=f1
+            f1=f2
+            f2=f-f1
+            offset=i
+        elif arr[i]>target:
+            f=f2
+            f1=f1-f2
+            f2=f-f1
+        else:
+            return i
+    if f1=1 and arr[offset+1]==target:
+        return offset+1
+    return -1
+               
