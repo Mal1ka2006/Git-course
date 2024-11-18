8e0d4719f8eb425b9218619e9ec82a7d933d9439
Malika Axmatova <malikaaxmatova06@gmail.com>
2024-11-18 15:24:12 +0500

    add merge.py

---
+++ merge.py
@@ -0,0 +1,11 @@
+def merge(left,right):
+    result=[]
+    i=0
+    j=0
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
